7448cbf865023358f9db37a150150d5992ef9e95
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-17 10:36:00 +0700

    membuat 1 row dengan 2 column, 1 column 3 komponen, 1 column 2 komponen

--- app/src/main/java/com/example/basiclayerkotlin/Tataletak1.kt
+++ app/src/main/java/com/example/basiclayerkotlin/Tataletak1.kt
@@ -107,3 +107,24 @@ fun TataletakColumRow(modifier: Modifier = Modifier) {
     }
 }
 
+
+@Composable
+fun TataletakRowColum(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        // Kolom1
+        Column {
+            Text(text = "KomponenKolom1")
+            Text(text = "KomponenKolom1s2")
+            Text(text = "KomponenKolom1s3")
+        }
+
+        // Kolom2
+        Column {
+            Text(text = "KomponenKolom2")
+            Text(text = "KomponenKolom2s2")
+        }
+    }
+}
